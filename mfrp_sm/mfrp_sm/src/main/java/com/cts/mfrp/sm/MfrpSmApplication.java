ebfcc992f2729b674e5058cc2229d7af864d45b8
2494359 <karolinebenezer@gmail.com>
2026-05-14 16:06:54 +0530

    Created project structure

---
+++ mfrp_sm/mfrp_sm/src/main/java/com/cts/mfrp/sm/MfrpSmApplication.java
@@ -0,0 +1,13 @@
+package com.cts.mfrp.sm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfrpSmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfrpSmApplication.class, args);
+	}
+
+}
---
+++ mfrp_sm/mfrp_sm/src/test/java/com/cts/mfrp/sm/MfrpSmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.mfrp.sm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfrpSmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
